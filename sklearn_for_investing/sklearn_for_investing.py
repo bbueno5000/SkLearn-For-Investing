cca397673e8e90c1f5808f33beeed59221dca99b
B. Bueno <bbueno25@hotmail.com>
2020-02-21 21:27:23 -0700

    Scikit Learn Machine Learning SVM Tutorial with Python p. 2 - Example

--- sklearn_for_investing/sklearn_for_investing.py
+++ sklearn_for_investing/sklearn_for_investing.py
@@ -1,1 +1,16 @@
+"""
+DOCSTRING
+"""
+from matplotlib import pyplot
+from sklearn import datasets
+from sklearn import svm
 
+dataset = datasets.load_digits()
+classifier = svm.SVC(gamma=0.001, C=100)
+x = dataset.data[:-1]
+y = dataset.target[:-1]
+classifier.fit(x, y)
+prediction = classifier.predict(dataset.data[-1])
+print('Prediction:', prediction)
+pyplot.imshow(dataset.images[-1], cmap=pyplot.cm_gray_r, interpoation='nearest')
+pyplot.show()
