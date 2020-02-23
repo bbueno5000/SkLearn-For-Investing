3828efd51ccfe21760faf2c01382cad4ea32cf08
B. Bueno <bbueno25@hotmail.com>
2020-02-23 10:34:05 -0700

    Scikit Learn Machine Learning Tutorial for investing with Python p. 5

--- sklearn_for_investing/sklearn_for_investing.py
+++ sklearn_for_investing/sklearn_for_investing.py
@@ -5,34 +5,42 @@ import datetime
 import os
 import time
 import pandas
+import sklearn
 from matplotlib import pyplot
-from sklearn import datasets
-from sklearn import svm
 
 def introduction():
     """
     DOCSTRING
     """
-    dataset = datasets.load_digits()
-    classifier = svm.SVC(gamma=0.001, C=100)
+    dataset = sklearn.datasets.load_digits(return_X_y=True)
+    classifier = sklearn.svm.SVC(gamma=0.001, C=100)
     variable_x = dataset.data[:-1]
     variable_y = dataset.target[:-1]
     classifier.fit(variable_x, variable_y)
     prediction = classifier.predict(dataset.data[-1])
     print('Prediction:', prediction)
-    pyplot.imshow(dataset.images[-1], cmap=pyplot.cm_gray_r, interpoation='nearest')
+    pyplot.imshow(dataset.images[-1], cmap=pyplot.get_cmap('gray'), interpoation='nearest')
     pyplot.show()
 
 def key_statistics(gather='Total Debt/Equity (mrq)'):
     """
     DOCSTRING
     """
-    filepath = 'intraQuarter'
-    statistics_path = os.path.join(filepath, ' KeyStats')
+    statistics_path = 'intraQuarter/_KeyStats'
     stock_list = [x[0] for x in os.walk(statistics_path)]
     for directory in stock_list[1:]:
         files = os.listdir(directory)
+        ticker = directory.split('\\')[1]
         if len(files) > 0:
             for file in files:
-                datestamp = datetime.strptime(file, '%Y%m&d%H%M%S.html')
+                datestamp = datetime.datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(datestamp.timetuple())
+                full_filepath = os.path.join(directory, file)
+                source = open(full_filepath, 'r').read()
+                value = source.split(gather+':</td><td class="yfnc_tabledata1">')[1]
+                value = value.split('</td>')[0]
+                print(ticker + ':', value)
+            time.sleep(15)
+
+if __name__ == '__main__':
+    key_statistics()
